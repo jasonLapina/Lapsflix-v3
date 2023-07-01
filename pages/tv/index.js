c59753f178815403ce869ae8845ce7ad8fbe18ff
jasonLapina <lapina.jason@gmail.com>
2023-07-01 13:35:45 +0800

    /tv page content

--- pages/tv/index.js
+++ pages/tv/index.js
@@ -1,6 +1,29 @@
-import { Box } from "@chakra-ui/react";
-function index() {
-  return <Box>tv naman dito hahaha</Box>;
+import { Box, Heading } from "@chakra-ui/react";
+import AvailableMovies from "../../components/Movies/AvailableMovies";
+import axiosInstance from "../../shared/axiosInstance";
+
+function index({ series }) {
+  return (
+    <Box>
+      <Heading textAlign='center' mb='40px'>
+        Discover TV Series
+      </Heading>
+      <AvailableMovies movies={series} />;
+    </Box>
+  );
 }
 
 export default index;
+
+export async function getStaticProps() {
+  const { data } = await axiosInstance.get("/discover/tv");
+  const { results, page, total_pages, total_results } = await data;
+  return {
+    props: {
+      page,
+      series: results,
+      total_pages,
+      total_results,
+    },
+  };
+}
