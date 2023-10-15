9934435c75f425dc81fa017bee2f8ec1cb3c2918
jasonLapina <lapina.jason@gmail.com>
2023-10-16 00:47:51 +0800

    add footer

--- shared/Layout/Footer.js
+++ shared/Layout/Footer.js
@@ -1,17 +1,31 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 function Footer() {
   return (
     <Box px='64px' py='32px' bgColor='#141414' h='30vh'>
-      <Box fontSize='40px'>
-        LapsFlix, movie browsing web app created by
-        <Text
-          as='a'
-          href='https://jasonlapina.com'
-          target='_blank'
-          rel='noreferrer'
+      <Box color='gray' fontSize='40px'>
+        <Heading
+          bgClip='text'
+          bgImage={"linear-gradient(to right,crimson 40%,cyan)"}
+          w='fit-content'
+          mb='24px'
         >
-          Jason Lapina.
-        </Text>
+          LAPSFLIX
+        </Heading>
+        <Box>
+          <Text fontSize='16px' w='fit-content'>
+            Demo web app to explore movies/series and see {"what's"} trending,
+            upcoming, etc.
+          </Text>
+          <Text
+            as='a'
+            href='https://jasonlapina.com'
+            target='_blank'
+            rel='noreferrer'
+            fontSize={{ base: "24px", md: "40px" }}
+          >
+            &copy; Jason Lapina.
+          </Text>
+        </Box>
       </Box>
     </Box>
   );
